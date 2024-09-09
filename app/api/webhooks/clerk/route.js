4b6d4ae1b9aff766363eb08c25ba5c29e946025c
mhmitas <mahfuzulmitas@gmail.com>
2024-09-09 06:19:40 +0600

    try to fix some issue with clerkclien and string

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -2,8 +2,7 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { createUser, deleteUser } from '@/lib/actions/user.action'
 import { NextResponse } from 'next/server'
-import { clerkClient } from '@clerk/nextjs/dist/types/server'
-
+import { clerkClient } from '@clerk/nextjs/server'
 
 export async function POST(req) {
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
